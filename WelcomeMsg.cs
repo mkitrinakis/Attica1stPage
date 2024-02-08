9c953412ae042fb35917be5a45f8b42ed865177d
Μάρκος Κιτρινάκης <mkitrinakis@minedu.gov.gr>
2024-02-08 12:00:46 +0200

    temp

---
+++ WelcomeMsg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using System.Web;
+using Microsoft.SharePoint.Utilities;
+
+namespace FirstPage
+{
+    public static class WelcomeMsg
+    {
+        public static string getUserName()
+        {
+            SPWeb web = SPContext.Current.Web;
+            return web.CurrentUser.Name; 
+
+        }
+    }
+}
